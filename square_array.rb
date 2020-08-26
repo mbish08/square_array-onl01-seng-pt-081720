fa2c67c9f5f174d43eb06a88eaf43e79ef0ffe04
Mikel Bishop <mdrew8@gmail.com>
2020-08-26 01:24:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  square_array.each{|arr| puts (arr * arr)|}
+  square_array.each{|arr| puts (arr * arr)}
 end
 
 #brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
