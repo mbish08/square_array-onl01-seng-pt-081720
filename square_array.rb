c0504bc634361ea12463b0313049c3717ab55904
Mikel Bishop <mdrew8@gmail.com>
2020-08-26 01:26:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,12 @@
 def square_array(array)
-  square_array.each{|arr| puts (arr * arr)}
+  square_array.each do |numbers|
+    puts numbers * numbers
+  end
 end
 
 #brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
 
 #brothers = ["Tim", "Tom", "Jim"]
-brothers.each do |brother|
-  puts "Stop hitting yourself #{brother}!"
-end
+#brothers.each do |brother|
+ # puts "Stop hitting yourself #{brother}!"
+#end
